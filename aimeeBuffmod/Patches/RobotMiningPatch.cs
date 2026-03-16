729ebfe17011fc3dd2f3d8c13b7ebb193266b091
Storm <tjaccardi98@gmail.com>
2026-03-16 00:55:17 -0400

    Added storm damage scale, motor torque config options. Set version to 2.2.0.0.

--- aimeeBuffmod/Patches/RobotMiningPatch.cs
+++ aimeeBuffmod/Patches/RobotMiningPatch.cs
@@ -21,40 +21,18 @@ namespace StormsAimeeBuff
 
             //patch non static publics
             __instance.MaxSpeed = aimeeBuffModPlugin.aimeeSpeed.Value;
-
-            //need to figure out how to implement this
-            //__instance.WeatherDamageScale = aimeeStormDamage.Value;
+            __instance.TargetMotorPower = aimeeBuffModPlugin.aimeeTorque.Value;
+            __instance.WeatherDamageScale = aimeeBuffModPlugin.aimeeStormDamage.Value;
 
             //patch static publics
             RobotMining.RepairSpeedScale = aimeeBuffModPlugin.aimeeRepairSpeed.Value;
             
-            aimeeBuffModPlugin.Log.LogInfo("Aimee speed set to " + __instance.MaxSpeed);
-
             //patch private static fields with reflection
             var type = typeof(RobotMining);
-            if(type.GetField("maxMiningDepth", BindingFlags.NonPublic | BindingFlags.Static) != null)
-            {
-                type.GetField("maxMiningDepth", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, aimeeBuffModPlugin.aimeeMiningDepth.Value);
-                aimeeBuffModPlugin.Log.LogInfo("Aimee mining depth set to " + type.GetField("maxMiningDepth", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null));
-            }
-
-            if(type.GetField("_isStuckCheckAmount", BindingFlags.NonPublic | BindingFlags.Static) != null)
-            {
-                type.GetField("_isStuckCheckAmount", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, aimeeBuffModPlugin.aimeeStuckTimer.Value);
-                aimeeBuffModPlugin.Log.LogInfo("Aimee stuck timer set to " + type.GetField("_isStuckCheckAmount", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null));
-            }
-
-            if(type.GetField("_isStuckMovementAmount", BindingFlags.NonPublic | BindingFlags.Static) != null)
-            {
-                type.GetField("_isStuckMovementAmount", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, aimeeBuffModPlugin.aimeeStuckSpeed.Value);
-                aimeeBuffModPlugin.Log.LogInfo("Aimee stuck speed set to " + type.GetField("_isStuckMovementAmount", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null));
-            }
-
-            if(type.GetField("MinableSearchArea", BindingFlags.NonPublic | BindingFlags.Static) != null)
-            {
-                type.GetField("MinableSearchArea", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, aimeeBuffModPlugin.aimeeSearchArea.Value);
-                aimeeBuffModPlugin.Log.LogInfo("Aimee search area set to " + type.GetField("MinableSearchArea", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null));
-            }
+            type.GetField("maxMiningDepth", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, aimeeBuffModPlugin.aimeeMiningDepth.Value);
+            type.GetField("_isStuckCheckAmount", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, aimeeBuffModPlugin.aimeeStuckTimer.Value);  
+            type.GetField("_isStuckMovementAmount", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, aimeeBuffModPlugin.aimeeStuckSpeed.Value);
+            type.GetField("MinableSearchArea", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, aimeeBuffModPlugin.aimeeSearchArea.Value);
         }
     }
 }
