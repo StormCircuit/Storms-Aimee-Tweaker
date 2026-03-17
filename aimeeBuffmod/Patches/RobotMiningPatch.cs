a159e900fc5da8cd447558504d571af46e41f029
Storm <tjaccardi98@gmail.com>
2026-03-17 18:41:54 -0400

    Updated thumb and preview, working on wind scaling

--- aimeeBuffmod/Patches/RobotMiningPatch.cs
+++ aimeeBuffmod/Patches/RobotMiningPatch.cs
@@ -4,6 +4,7 @@ using BepInEx.Logging;
 using HarmonyLib;
 using System.Reflection;
 using Assets.Scripts.Objects;
+using UnityEngine;
 //aimee tweaker by Storm and Copilot, written with help from the modding community
 
 
@@ -16,7 +17,6 @@ namespace StormsAimeeBuff
         [HarmonyPostfix]
         public static void aimeeBuffPatch(RobotMining __instance)
         {
-
             aimeeBuffModPlugin.Log.LogInfo("Patching aimee values.");
 
             //patch non static publics
@@ -33,6 +33,20 @@ namespace StormsAimeeBuff
             type.GetField("_isStuckCheckAmount", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, aimeeBuffModPlugin.aimeeStuckTimer.Value);  
             type.GetField("_isStuckMovementAmount", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, aimeeBuffModPlugin.aimeeStuckSpeed.Value);
             type.GetField("MinableSearchArea", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, aimeeBuffModPlugin.aimeeSearchArea.Value);
+        }
+    }
+
+    [HarmonyPatch(typeof(DynamicThing))]
+    public static class DynamicThingPatcher
+    {
+        [HarmonyPatch("GetStormWindVector")]
+        [HarmonyPostfix]
+        public static Vector3 GetStormWindVector_Postfix(Vector3 __result, DynamicThing __instance)
+        {
+            if(__instance is RobotMining){
+                return __result * aimeeBuffModPlugin.aimeeWindScale.Value;
+            }
+            return __result;
         }
     }
 }
